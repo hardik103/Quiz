db9e6de024b72b312c5cb7eee438a076d7436d52
Hardik <hardikpandey103@gmail.com>
2023-04-15 01:43:06 +0530

    First Commit

---
+++ quiz_js.js
@@ -0,0 +1,9 @@
+var total;
+
+// to add questions received from server to the html document
+function adder(count,data[]){
+    for(i=0;i<count;i++){
+        let document = data[i];
+        document.getElementById("body").innerHTML += 
+    }
+}
---
+++ quiz_server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+var port=8080;
+
+app.get('/',(request,response) => {
+    response.sendFile(__dirname+'/quiz.html');
+});
+
+app.get('/result',(request,response) => {
+    var marksA=0,marksB=0,marksC=0;
+    const setA=['c','a','b','c','a','a','b','c','a','b'];
+    const setB=['a','c','b','d','a','c','d','a','b','d'];
+    const setC=['d','a','a','b','b','c','a','c','d','b'];
+    for(var i=0;i<10;i++) {
+        if(request.query[`${i+1}`] == setA[i] ) marksA++;
+        if(request.query[`${i+11}`] == setB[i] ) marksB++;
+        if(request.query[`${i+21}`] == setC[i] ) marksC++;
+    }
+    response.setHeader( 'content-type' , 'text/html' )
+    response.write(`Set A : ${marksA}/10<br>`);
+    response.write(`Set B : ${marksB}/10<br>`);
+    response.write(`Set C : ${marksC}/10<br>`);
+    response.write(`Your score is ${ marksA+marksB+marksC }/30.`);
+    response.end();
+});
+
+
+app.listen(port ,() => {
+    console.log(`http://localhost:${port}/`);
+});
